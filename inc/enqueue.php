b817cac1161e5aab37042ffec2596af1899c3b37
Mario Joseph <mariocjoseph@gmail.com>
2021-01-27 14:27:28 +0100

    20210127v1

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -6,7 +6,8 @@ function shoe_load_admin_scripts($hook){
 
     // echo $hook;
 
-    if ('toplevel_page_shoePostCtrl' != $hook){return;}
+
+    if (('toplevel_page_shoePublished' == $hook)||('toplevel_page_shoePostCtrl' == $hook)||('toplevel_page_shoeDraft' == $hook)){
 
 // wp_register_style('shoe_admin', get_theme_file_path('/css/admin1.css'), array(),'1.0.0', 'all' );
 wp_register_style('shoe_admin', get_stylesheet_directory_uri().'/css/admin/admin1.css', array(),'1.0.0', 'all' );
@@ -15,7 +16,12 @@ wp_register_style('shoe_admin', get_stylesheet_directory_uri().'/css/admin/admin
   wp_localize_script('shoe_admin', 'shoeData1', array(
     'root_url' => get_site_url(),
     'nonce' => wp_create_nonce('wp_rest')
-));
+));}
+
+else{
+    return;
+}
+
 }
 
 add_action('admin_enqueue_scripts', 'shoe_load_admin_scripts');
