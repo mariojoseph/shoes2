0d511310a079ad8b9b690dd9ddafb7fd0d88063b
Mario Joseph <mariocjoseph@gmail.com>
2021-03-02 10:51:51 +0100

    20210203v2

--- functions.php
+++ functions.php
@@ -385,4 +385,14 @@ function data_fetch(){
 
 // END OF TESTING AJAX 20200926
 
+// BEGINNING OF TESTING EMAIL CHANGES
 
+add_filter(  'gettext',  'register_text'  );
+add_filter(  'ngettext',  'register_text'  );
+function register_text( $translated ) {
+     $translated = str_ireplace(  'Username or Email Address',  'CARA SABE MUITO',  $translated );
+     return $translated;
+}
+
+
+// END OF TESTING EMAIL CHANGES
